ec6099f419d45541384b3141011b2e902af3f0d4
Volodymyr Korolevych <volodymyr.korolevych@gmail.com>
2026-05-26 15:52:28 +0300

    footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,7 +2,7 @@
   <footer class="mt-12 border-t border-border bg-white">
     <div class="max-w-6xl mx-auto px-6">
      <div
-        class="min-h-[60px] py-6 text-[18px] text-primary font-semibold flex items-center justify-center">
+        class="min-h-[60px] py-6 text-[18px] text-black font-semibold flex items-center justify-center">
         <p>© 2026 Пекарня Зерно</p>
       </div>
     </div>
